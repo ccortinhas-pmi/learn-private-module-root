3bc40c1e848eb7e3a9fafc91af199138f62f1192
ccortinhas-pmi <83591746+ccortinhas-pmi@users.noreply.github.com>
2021-05-03 15:03:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/tutorial_ccort/s3-webapp/aws"
+  source  = "app.terraform.io/tutorial_ccort/terraform/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
